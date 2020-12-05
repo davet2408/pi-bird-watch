22430152bac7232d2c0574d01a7bbb46f4c8e05f
David Temple <david.temple@kcl.ac.uk>
2020-12-05 21:27:57 +0000

    Add index page that wil display the stream

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
-    return "Hello World!"
+def home_page():
+    return render_template('index.html')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return 'video.png'
+
 
 if __name__ == '__main__':
     app.run()
