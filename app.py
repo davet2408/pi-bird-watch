c6d8a76a81f2ede878bc67535bcf4848356f0154
David Temple <david.temple@kcl.ac.uk>
2020-12-06 16:49:30 +0000

    Add placeholder for when no video feed avialable

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 app = Flask(__name__)
+
+camera = None
 
 
 @app.route('/')
 def home_page():
-    return render_template('index.html')
+    return render_template('index.html', camera=camera)
 
 
 @app.route('/video_feed')
 def video_feed():
-    return 'video.png'
+    return Response()
 
 
 if __name__ == '__main__':
